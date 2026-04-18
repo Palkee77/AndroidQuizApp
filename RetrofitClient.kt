ab1578c2bc36708e98f9fe31bc79ab0882f917cd
Palkee77 <ervinpalic69@gmail.com>
2026-04-18 18:56:04 +0200

    Add RetrofitClient.kt

---
+++ RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.example.androidquizapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.example.com/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getService(): ApiService = retrofit.create(ApiService::class.java)
+}
